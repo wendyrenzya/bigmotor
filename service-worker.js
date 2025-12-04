60c8a312d928bd801dbb73f8a85e4eca27ef3858
wendyrenzya <wendyrenzya@gmail.com>
2025-12-04 12:50:39 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,56 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
+const CACHE_NAME = "bigmotor-cache-v1";
+
+const ASSETS = [
+  "/", 
+  "/index.html",
+  "/home.html",
+  
+  // semua file html penting
+  "/absen.html",
+  "/add_item.html",
+  "/app.js",
+  "/audit.html",
+  "/barang.html",
+  "/barang_detail.html",
+  "/barang_edit.html",
+  "/bonus.html",
+  "/buat_katalog.html",
+  "/harga.html",
+  "/katalog.html",
+  "/laporan.html",
+  "/laporan_harian.html",
+  "/list_katalog.html",
+  "/navigationGuard.js",
+  "/pengaturan.html",
+  "/pengeluaran.html",
+  "/penjualan.html",
+  "/riwayat.html",
+  "/servis.html",
+  "/servis_detail.html",
+  "/stok_masuk.html",
+  "/struk.html",
+  "/tambah_barang.html",
+  "/tambah_servis.html",
+  "/user.html",
+
+  // asset
+  "/assets/logo.png",
+  "/assets/icon-192.png",
+  "/assets/icon-512.png"
+];
+
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache=>{
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => {
-    return caches.match('index.html');
-  }));
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(resp=>{
+      return resp || fetch(e.request);
+    })
+  );
 });
